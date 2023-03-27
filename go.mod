52af52c6860b6b7e09b39f0f2bfacd8eb29043b2
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2023-03-27 10:14:56 +0200

    Test ProcessConfig

--- go.mod
+++ go.mod
@@ -5,9 +5,14 @@ go 1.19
 require (
 	github.com/bitrise-io/go-steputils/v2 v2.0.0-alpha.17
 	github.com/bitrise-io/go-utils/v2 v2.0.0-alpha.16
+	github.com/stretchr/testify v1.8.2
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ step/step_test.go
@@ -0,0 +1,121 @@
+package step
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/bitrise-io/go-steputils/v2/stepconf"
+	"github.com/bitrise-io/go-utils/v2/log"
+	"github.com/bitrise-steplib/bitrise-step-share-env-vars-between-stages/mocks"
+)
+
+func TestEnvVarSharer_ProcessConfig(t *testing.T) {
+	tests := []struct {
+		name    string
+		envs    map[string]string
+		want    *Config
+		wantErr bool
+	}{
+		{
+			name: "Simple inputs",
+			envs: map[string]string{
+				"env_vars":        "MY_ENV_KEY=my value",
+				"app_url":         "https://app.bitrise.io/app/abcd",
+				"build_slug":      "asdf",
+				"build_api_token": "1234",
+			},
+			want: &Config{
+				EnvVars:       []EnvVar{{Key: "MY_ENV_KEY", Value: "my value"}},
+				AppURL:        "https://app.bitrise.io/app/abcd",
+				BuildSlug:     "asdf",
+				BuildAPIToken: "1234",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Existing env sharing",
+			envs: map[string]string{
+				"EXISTING_ENV_KEY": "existing env",
+				"env_vars":         "EXISTING_ENV_KEY",
+				"app_url":          "https://app.bitrise.io/app/abcd",
+				"build_slug":       "asdf",
+				"build_api_token":  "1234",
+			},
+			want: &Config{
+				EnvVars:       []EnvVar{{Key: "EXISTING_ENV_KEY", Value: "existing env"}},
+				AppURL:        "https://app.bitrise.io/app/abcd",
+				BuildSlug:     "asdf",
+				BuildAPIToken: "1234",
+			},
+			wantErr: false,
+		},
+		{
+			name: "env_vars is required",
+			envs: map[string]string{
+				"env_vars":        "",
+				"app_url":         "https://app.bitrise.io/app/abcd",
+				"build_slug":      "asdf",
+				"build_api_token": "1234",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "app_url is required",
+			envs: map[string]string{
+				"env_vars":        "MY_ENV_KEY=my value",
+				"app_url":         "",
+				"build_slug":      "asdf",
+				"build_api_token": "1234",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "build_slug is required",
+			envs: map[string]string{
+				"env_vars":        "MY_ENV_KEY=my value",
+				"app_url":         "https://app.bitrise.io/app/abcd",
+				"build_slug":      "",
+				"build_api_token": "1234",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "build_api_token is required",
+			envs: map[string]string{
+				"env_vars":        "MY_ENV_KEY=my value",
+				"app_url":         "https://app.bitrise.io/app/abcd",
+				"build_slug":      "asdf",
+				"build_api_token": "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			envRepository := new(mocks.Repository)
+			for key, value := range tt.envs {
+				envRepository.On("Get", key).Return(value)
+			}
+
+			inputParser := stepconf.NewInputParser(envRepository)
+
+			e := EnvVarSharer{
+				logger:        log.NewLogger(),
+				inputParser:   inputParser,
+				envRepository: envRepository,
+			}
+			got, err := e.ProcessConfig()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ProcessConfig() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ProcessConfig() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
